1a12c53ffb9009cfc0b54fb6ea6776035c6ccb59
LuisDomingos <luis.domingos@b-i.com>
2018-03-23 14:08:58 +0000

    Initial commit

---
+++ src/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>{{ display }}</div>
+    <p>{{msg}}</p>
+</template>
+<script>
+    import moment from 'moment';
+
+    export default {
+        data() {
+            return {
+                time: Date.now()
+            }
+        },
+        props: ['msg', 'onUpdate'],
+        computed: {
+            display() {
+                this.onUpdate()
+                return moment(this.time).format("HH:mm:ss");
+            }
+        },
+        created() {
+            setInterval(() => {
+                this.time = Date.now();
+            }, 1000);
+        }
+    }
+</script>
